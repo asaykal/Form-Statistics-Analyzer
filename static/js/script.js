4609aa769c647da0a47c66472b0e13c209a796e4
Abdulmuttalip Saykal <46647858+asaykal@users.noreply.github.com>
2023-05-09 22:31:50 +0300

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,30 +1,57 @@
 $(document).ready(function() {
-    $('#form').submit(function(event) {
-      event.preventDefault();
-  
-      var formData = {
-        time: $('#time').val(),
-        mission: $('#mission').val(),
-        goal: $('#goal').val(),
-        goal_point: $('#goal-point').val(),
-        pleasure_point: $('#pleasure-point').val(),
-        notes: $('#notes').val()
-      };
-      
-      $.ajax({
-        type: 'POST',
-        url: '/form',
-        data: JSON.stringify(formData),
-        contentType: 'application/json',
-        success: function(response) {
-          alert(response.response);
-          $('#form')[0].reset(); 
-        },
-        error: function(error) {
-          console.log(error);
-          alert('An error occurred. Please try again.');
-        }
-      });
+  $('#form').submit(function(event) {
+    event.preventDefault();
+
+    var formData = {
+      time: $('#time').val(),
+      mission: $('#mission').val(),
+      goal: $('#goal').val(),
+      goal_point: $('#goal-point').val(),
+      pleasure_point: $('#pleasure-point').val(),
+      notes: $('#notes').val()
+    };
+
+    $.ajax({
+      type: 'POST', 
+      url: '/form',
+      data: JSON.stringify(formData),
+      contentType: 'application/json',
+      success: function(response) {
+        alert(response.response);
+        $('#form')[0].reset();
+      },
+      error: function(error) {
+        console.log(error);
+        alert('An error occurred. Please try again.');
+      }
     });
   });
-  
+
+  $('#delete-form').submit(function(event) {
+    event.preventDefault();
+
+    var formData = {
+      time: $('#time').val(),
+      mission: $('#mission').val(),
+      goal: $('#goal').val(),
+      goal_point: $('#goal-point').val(),
+      pleasure_point: $('#pleasure-point').val(),
+      notes: $('#notes').val()
+    };
+
+    $.ajax({
+      type: 'POST', 
+      url: '/delete',
+      data: JSON.stringify(formData),
+      contentType: 'application/json',
+      success: function(response) {
+        alert(response.response);
+        $('#form')[0].reset();
+      },
+      error: function(error) {
+        console.log(error);
+        alert('An error occurred. Please try again.');
+      }
+    });
+  });
+});
